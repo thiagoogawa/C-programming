f45edb7d94bf47046fc7b91ea141afad279bdc8f
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-04-18 20:57:33 -0300

    commit

--- Advanced C programming/Addresses and Indirection/05.c
+++ Advanced C programming/Addresses and Indirection/05.c
@@ -3,5 +3,23 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  
+  int i;
+
+  for(int i = 0; i < argc; i++) {
+    printf("arg %d is %s\n", i, argv[i]); //prints arg at index i
+  }
+
+  printf("\n\n");
+
+  for(i = 0; i< argc; i++) {
+    printf("arg %d is %s\n", i, *argv); //prints arg pointed to by argv
+    argv += 1;
+  }
+
+  // arg 0 is CommandLineArgs
+  // arg 1 is one
+  // arg 2 is two
+  // arg 3 is three
+
+  return 0;
 }
---
+++ Advanced C programming/Structures/01.c
@@ -0,0 +1,28 @@
+// creating a structure template
+
+// struct <struct-name>
+// {
+//   <type><field_name>;
+//   <type><field_name>;
+//   <type><field_name>;
+// }
+
+// example:
+// struct date {
+//   int day;
+//   int month;
+//   int year;
+// };
+
+// struct point {
+//   int x;
+//   int y;
+// };
+
+// struct address {
+//   char state[20];
+//   char city[10];
+//   char street[15];
+//   int number;
+// }
+
